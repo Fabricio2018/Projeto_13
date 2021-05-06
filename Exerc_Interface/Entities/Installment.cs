f40ec05cb59f846247300d087ebc7d4146c95bb7
Fabrício Oliveira Dias <fabricio.dias102@gmail.com>
2021-05-06 15:25:43 -0300

    Exercicio sobre parcelas de contrato de uma empresa utilizando interfaces

---
+++ Exerc_Interface/Entities/Installment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exerc_Interface.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            this.DueDate = dueDate;
+            this.Amount = amount;
+        }
+
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy")
+                + " - "
+                + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
